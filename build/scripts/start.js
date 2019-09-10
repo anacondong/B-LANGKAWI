2ff631b66ac47f44db70215749c676a757f5bb28
ittipol jannapast <ittipol.jannapast@nbc.ca>
2019-09-10 02:01:08 -0400

    update
    update

--- build/scripts/start.js
+++ build/scripts/start.js
@@ -1,6 +1,6 @@
 const logger = require('../lib/logger')
 
 logger.info('Starting server...')
-require('../../server/main').listen(process.env.PORT || 8080, () => {
-  logger.success('Server is running at http://localhost:8080')
+require('../../server/main').listen(process.env.PORT || 3000, () => {
+  logger.success('Server is running at http://localhost:3000')
 })
